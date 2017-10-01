4e17fdc8a281c384d6f98cba9c3286ca41d1e2b9
David Antliff <david@pitchblende.co.nz>
2017-10-01 13:58:10 +1300

    Add rmt.h.

---
+++ include/esp32-utils/rmt.h
@@ -0,0 +1,58 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 David Antliff
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * @file rmt.h
+ * @brief RMT-related functions and definitions.
+ */
+
+#ifndef ESP32_UTILS_RMT_H
+#define ESP32_UTILS_RMT_H
+
+#include <stdint.h>
+#include "rmt.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Unfortunately ESP-IDF does not explicitly define a symbol with the bitfield width of an RMT RAM item.
+ */
+#define RMT_ITEM_DURATION_WIDTH 15
+
+/**
+ * @brief When looping, there are only N-1 items usable in an RMT mem block
+ */
+#define RMT_LOOP_MAX_ITEMS_PER_MEM_BLOCK (RMT_MEM_ITEM_NUM - 1)
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // ESP32_UTILS_RMT_H
+
+
+
